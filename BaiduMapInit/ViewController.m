771266742601b649c49f248ccdf4c32a32618319
王小朋 <monsters@yidianlingdeiMac.local>
2018-03-23 14:11:01 +0800

    整理代码

--- BaiduMapInit/ViewController.m
+++ BaiduMapInit/ViewController.m
@@ -8,17 +8,20 @@
 
 #import "ViewController.h"
 
+//优化后不需要再次导入繁琐的头文件，不需要在AppDelegate中有相关操作
 //#import <BaiduMapAPI_Base/BMKBaseComponent.h>//引入base相关所有的头文件
 //#import <BaiduMapAPI_Map/BMKMapComponent.h>//引入地图功能所有的头文件
 
 //#import <MAMapKit/MAMapKit.h>
 //#import <AMapFoundationKit/AMapFoundationKit.h>
 
-#import "BaiduMapView.h"
-#import "GaodeMapView.h"
-#import "BaiduMapFactory.h"
-#import "GaodeMapFactory.h"
 #import "MapEngine.h"
+
+#import "BaiduMapView.h"
+#import "BaiduMapFactory.h"
+
+#import "GaodeMapView.h"
+#import "GaodeMapFactory.h"
 
 @interface ViewController ()
 
@@ -33,6 +36,10 @@
 
     /**
      *  百度地图普通写法
+     *  缺点：
+     *  需要导入低头相关头文件
+     *  AppDelegate中需要导入头文件  需要注册地图
+     *  不能延后加载（使用地图的时候在加载）
      *
     BMKMapView* mapView = [[BMKMapView alloc]initWithFrame:self.view.bounds];
     self.view = mapView;
@@ -50,23 +57,9 @@
     [AMapServices sharedServices].enableHTTPS = YES;
     //初始化地图
     MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
-    //把地图添加至view
     [self.view addSubview:_mapView];
     */
     
-    /*
-    id<IMapView> mapView = [[GaodeMapView alloc]initWithFrame:self.view.frame];
-    [self.view addSubview:[mapView getView]];
-     */
-    
-//    代码结构进一步优化（解决方案：工厂模式->百度地图工厂设计）
-//    id<IMapFactory> factory = [[BaiduMapFactory alloc]init];
-//    id<IMapView> mapView = [factory getMapView:self.view.frame];
-//    [self.view addSubview:[mapView getView]];
-    
-//    id<IMapFactory> factory = [[GaodeMapFactory alloc]init];
-//    id<IMapView> mapView = [factory getMapView:self.view.frame];
-//    [self.view addSubview:[mapView getView]];
     
     /**
      *  代码结构最终优化（解决方案：工厂模式->地图切换分析、实现原理）
@@ -74,18 +67,20 @@
      *  第一个方案：通过宏定义（枚举）实现
      *  第二个方案：配置文件（xml配置、josn配置文件等。。。）
      */
-     //第一个方案：通过宏定义（枚举）实现
-//    MapEngine *mapEngine = [[MapEngine alloc] init];
-//    id<IMapFactory> factory = [mapEngine getFactory:MapTypeBaiduMap];
-//    id<IMapView> mapView = [factory getMapView:self.view.frame];
-//    [self.view addSubview:[mapView getView]];
     
-    //第二个方案：配置文件（xml配置、josn配置文件等。。。）
+    
+     //第一个方案：通过宏定义（枚举）实现
     MapEngine *mapEngine = [[MapEngine alloc] init];
-//    [mapEngine initMap];
-    id<IMapFactory> factory = [mapEngine getFactory];
+    id<IMapFactory> factory = [mapEngine getFactory:MapTypeBaiduMap];
     id<IMapView> mapView = [factory getMapView:self.view.frame];
     [self.view addSubview:[mapView getView]];
+    
+    
+    //第二个方案：配置文件（xml配置、josn配置文件等。。。）
+//    MapEngine *mapEngine = [[MapEngine alloc] init];
+//    id<IMapFactory> factory = [mapEngine getFactory];
+//    id<IMapView> mapView = [factory getMapView:self.view.frame];
+//    [self.view addSubview:[mapView getView]];
 
 }
 
